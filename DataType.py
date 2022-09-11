1f99e25227a4c5461aafeb95d4186c999b4ca303
bntg <111212784+bintangry@users.noreply.github.com>
2022-09-11 14:54:32 +0700

    first commit

---
+++ DataType.py
@@ -0,0 +1,23 @@
+# ini tipe data string
+nama = "Budi"
+# ini tipe data integer
+umur = 17 
+# ini tipe data float
+nilaimatematika= 7.5
+# ini tipe data array
+barang_bawaan= ["sepatu", "kaos","Celana"]
+# ini tipe data boolean
+Jawaban= True
+
+print("halo nama saya ", nama)
+
+print("umur saya", umur)
+
+print( "barang yang saya bawa  ada", barang_bawaan)
+
+print("jawaban saya", Jawaban)
+
+print("tipe data nama adalah", type(nama))
+print("tipe data umur adalah", type(umur))
+print("tipe data barang yang saya bawa adalah",type(barang_bawaan))
+print("tipe daata jawaban adalah",type(Jawaban))
---
+++ variabel.py
@@ -0,0 +1,13 @@
+# nama variable adalah tempat menampung nilai sementara 
+#nama variable bebas
+from cgi import print_directory
+
+
+bilangan_pertama = 10
+bilangan_kedua = 20
+bilangan_ketiga = 90
+jumlah_tambah = bilangan_pertama + bilangan_kedua + bilangan_ketiga + 20
+print("bilangan pertama:" ,bilangan_pertama)
+print("bilangan kedua:" , bilangan_kedua)
+print("bilangan:", bilangan_ketiga)
+print("jumlah tambah :", jumlah_tambah)
