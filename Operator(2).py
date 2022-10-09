63372a9bc09ef035ebc080c7d7553a13adc63512
bntg <111212784+bintangry@users.noreply.github.com>
2022-10-09 14:32:36 +0700

    Logical comparison in python

---
+++ Operator(2).py
@@ -0,0 +1,13 @@
+a = int(input("masukan bilangan satu :"))
+b = int(input("masukan bilangan dua :"))
+
+# Arithmetic Operators
+
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a * b = {a * b}")
+print(f"a / b = {a / b}")
+ # modulus adalah sisa hasil bagi %
+print(f"a % b = {a % b}")
+print(f"a ** b {a ** b}")
+print(f"a // b {a // b}")
---
+++ Operator.py
@@ -0,0 +1,6 @@
+bil_1 = int(input("masukan bilangan satu"))
+bil_2 = int(input("masukan bilangan dua"))
+
+# Addition
+hasil_tambah = bil_1 + bil_2
+print("Hasil bilangan = " , hasil_tambah)
