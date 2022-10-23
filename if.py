22cb9405f9b887bfaacd1874cc5edf8f66d6e9fa
bntg <111212784+bintangry@users.noreply.github.com>
2022-10-23 14:27:03 +0700

    Belajar if else

---
+++ if.py
@@ -0,0 +1,10 @@
+num = int (input('Enter any number: '))
+if (num % 5 == 0):
+    print(f'Given number {num} is divisible by 5') 
+    print('BENAR')
+print('ini akan dicetak terus')
+print(f'{num % 5}')
+
+
+if (5 > 5 and 5 > 8):
+    print('Halo')
---
+++ ififElseElse.py
@@ -0,0 +1,29 @@
+from tkinter import E
+
+
+nilai = int (input('Enter any number:'))
+
+# jika nilai 90 ~ 100 maka mendapat nilai A 
+# jika nilai 80 ~ 89 maka mendapat nilai B
+# jika nilai 70 ~ 79 maka mendapat nilai C
+# jika nilai 60 ~ 69 maka mendapat nilai D
+# jika nilai kurang dari 60 mendapat nilai F
+
+
+if(nilai >= 90 and  nilai <=100 ): #jika nilai 90 ~100maka mendapatkan A
+    print( f'nilai {nilai} mendapatkan A') 
+elif (nilai >= 80 and nilai <90): 
+        print( f'nilai {nilai} mendapatkan Bukan A') 
+elif (nilai >= 70 and nilai < 80 ) :
+     print(f'nilai {nilai}mendapatkan C') 
+elif (nilai >=60 and nilai <70 ) :
+    print(f'nilai {nilai}  mendapatkan D')
+elif (nilai < 60) :
+    print(f'nilai {nilai}  mendapatkan F')
+else :
+    print(f'nilai {nilai} Tidak dalam Rentang')
+
+
+   
+
+    
